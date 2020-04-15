4816f6d86e4852f0865960bb9f3121f1a352e700
Jiahuan Yang <jiahuan.yang@thoughtworks.com>
2020-04-15 10:59:24 +0800

    feat: store bag in the locker with highest vacancy rate (second locker)

--- src/test/groovy/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.groovy
+++ src/test/groovy/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.groovy
@@ -19,4 +19,20 @@ class SuperLockerRobotTest extends Specification {
         ticket != null
         bag == firstLocker.take(ticket)
     }
+
+    void "should store the bag in the 1st locker when store bag given the super locker robot manages 2 lockers and the 1st locker's capacity is 4, vacancy rate is 50% while the 2nd locker's capacity is 1, vacancy rate is 100%"() {
+        given:
+        def firstLocker = new Locker(4)
+        def secondLocker = new Locker(1)
+        def superLockerRobot = new SuperLockerRobot([firstLocker, secondLocker])
+        (1..2).forEach({ firstLocker.store(new Bag()) })
+        def bag = new Bag()
+
+        when:
+        def ticket = superLockerRobot.store(bag)
+
+        then:
+        ticket != null
+        bag == secondLocker.take(ticket)
+    }
 }
